4fabb30907c25a087447088acdd65364336e1f1c
Shota Iwamoto <amaginqoo@gmail.com>
2019-12-25 23:37:46 -0800

    Add keyboad config

--- Nailish/LoginViewController.swift
+++ Nailish/LoginViewController.swift
@@ -51,11 +51,19 @@ class LoginViewController: UIViewController, UIApplicationDelegate, GIDSignInDel
 
         view.backgroundColor = .white
 
-        let btnSignIn = GIDSignInButton(frame: CGRect(x: 0, y: 0, width: 230, height: 50))
-        btnSignIn.center = view.center
-        btnSignIn.style = .wide
+        let logo = UIImageView(image: UIImage(named: "logo1"))
+        view.addSubview(logo)
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        logo.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        logo.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        logo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 
-        let inputSV = UIStackView(arrangedSubviews: [emailText, passwordText, loginButton, signUpButton, btnSignIn])
+        let googleBtnSignIn = GIDSignInButton(frame: CGRect(x: 0, y: 0, width: 230, height: 50))
+        googleBtnSignIn.center = view.center
+        googleBtnSignIn.style = .wide
+
+        let inputSV = UIStackView(arrangedSubviews: [emailText, passwordText, loginButton, googleBtnSignIn, orText, signUpButton])
         view.addSubview(inputSV)
         inputSV.axis = .vertical
         inputSV.spacing = 20
@@ -63,17 +71,43 @@ class LoginViewController: UIViewController, UIApplicationDelegate, GIDSignInDel
 
         inputSV.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
 
-        inputSV.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputSV.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 30).isActive = true
         inputSV.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillBeHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillBeShown), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
     @objc func LoginTapped() {
         mainTabBarController = MainTabBarController()
+        
+
+
+
         self.present(mainTabBarController, animated: true, completion: nil)
     }
 
     @objc func SignUpTapped() {
         self.present(SignUpViewController(), animated: true, completion: nil)
+    }
+
+    @objc func keyboardWillBeShown(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardFrameHeight = keyboardSize.cgRectValue.height
+
+        if passwordText.isFirstResponder {
+            self.view.frame.origin.y = -keyboardFrameHeight
+        }
+
+    }
+
+    @objc func keyboardWillBeHidden(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
     // UIParts
@@ -98,9 +132,16 @@ class LoginViewController: UIViewController, UIApplicationDelegate, GIDSignInDel
         return  tf
     }()
 
+    let orText: UILabel = {
+        let lb = UILabel()
+        lb.text = "or"
+        lb.textAlignment = .center
+        return lb
+    }()
+
     let signUpButton: UIButton = {
         let bt = UIButton()
-        bt.setTitle("Sing Up", for: .normal)
+        bt.setTitle("Sing up", for: .normal)
         bt.backgroundColor = .red
         bt.addTarget(self, action: #selector(SignUpTapped), for: .touchUpInside)
         return bt
--- Nailish/SignUpViewController.swift
+++ Nailish/SignUpViewController.swift
@@ -20,7 +20,15 @@ class SignUpViewController: UIViewController {
 
         view.backgroundColor = .white
 
-        let inputSV = UIStackView(arrangedSubviews: [emailText, passwordText, signUpButton])
+        let logo = UIImageView(image: UIImage(named: "logo1"))
+        view.addSubview(logo)
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        logo.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        logo.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        logo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        let inputSV = UIStackView(arrangedSubviews: [emailText, passwordText, repeatPasswordText, signUpButton, cancelButton])
         view.addSubview(inputSV)
         inputSV.axis = .vertical
         inputSV.spacing = 20
@@ -28,12 +36,25 @@ class SignUpViewController: UIViewController {
 
         inputSV.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
 
-        inputSV.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputSV.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 30).isActive = true
         inputSV.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillBeHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillBeShown), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
     @objc func SignUpTapped() {
         print("SignUpTapped")
+        if(emailText.text == "" || passwordText.text == "" || repeatPasswordText.text == "") {
+            displayMyAlertMessage(userMessage: "Please input all field")
+            return
+        }
+        if passwordText.text != repeatPasswordText.text {
+            displayMyAlertMessage(userMessage: "The password does not match!")
+            return
+        }
+
         if let email = emailText.text, let password = passwordText.text {
             Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
                 if (result?.user) != nil{
@@ -41,6 +62,36 @@ class SignUpViewController: UIViewController {
                     self.present(MainTabBarController(), animated: true, completion: nil)
                 }
             }
+        }
+    }
+
+    @objc func cancelTapped() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+    func displayMyAlertMessage(userMessage: String){
+        let myAlert = UIAlertController(title:"Alert", message: userMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title:"OK", style: .default, handler:nil)
+        myAlert.addAction(okAction);
+        self.present(myAlert,animated:true, completion:nil)
+    }
+
+    @objc func keyboardWillBeShown(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardFrameHeight = keyboardSize.cgRectValue.height
+
+        if (passwordText.isFirstResponder || repeatPasswordText.isFirstResponder) {
+            self.view.frame.origin.y = -keyboardFrameHeight
+        }
+
+    }
+
+    @objc func keyboardWillBeHidden(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
         }
     }
 
@@ -63,6 +114,18 @@ class SignUpViewController: UIViewController {
         tf.layer.cornerRadius = 5
         tf.autocapitalizationType = .none
         tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        return  tf
+    }()
+
+    let repeatPasswordText: MyTextField = {
+        let tf = MyTextField()
+        tf.backgroundColor = .white
+        tf.layer.borderWidth = 1
+        tf.layer.cornerRadius = 5
+        tf.autocapitalizationType = .none
+        tf.placeholder = "Repeat Password"
+        tf.isSecureTextEntry = true
         return  tf
     }()
 
@@ -74,6 +137,12 @@ class SignUpViewController: UIViewController {
         return bt
     }()
 
-
+    let cancelButton: UIButton = {
+        let bt = UIButton()
+        bt.setTitleColor(.blue, for: .normal)
+        bt.setTitle("Cancel", for: .normal)
+        bt.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
+        return bt
+    }()
 
 }
